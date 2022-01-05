cbcd0dfa09c9d6facb0f191bea48d54c55e8f82a
Natbud <96924057+Natbud@users.noreply.github.com>
2022-01-05 17:48:28 +0000

    Update 06_01.py

--- 06_01.py
+++ 06_01.py
@@ -2,7 +2,13 @@ import numpy as np
 
 filepath = "06_01_Test_Data.txt"
 
-with open(filepath) as f:
+with open(filepath, 'r') as f:
     file_list = f.readlines()
 
-print(file_list)
+print("file_list:", file_list)
+
+new_list = []
+
+for line in file_list:
+    new_list.append(line.strip())
+    print("new_list:", new_list)
