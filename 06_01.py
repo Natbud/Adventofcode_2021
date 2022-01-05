f21c270f971f261f45685474f6703d8c234bb6c3
Natbud <96924057+Natbud@users.noreply.github.com>
2022-01-05 20:55:05 +0000

    06_01 Done

--- 06_01.py
+++ 06_01.py
@@ -1,14 +1,26 @@
 import numpy as np
 
-filepath = "06_01_Test_Data.txt"
-
+filepath = "06_01_Data.txt"
 with open(filepath, 'r') as f:
     file_list = f.readlines()
+    file_list = [line.strip() for line in file_list]
 
-print("file_list:", file_list)
+# take values inbetween commas and insert into new list
+fish_list = [line.split(",") for line in file_list]
+# alter to alist of interger types rather than strings:
+fish_list = [int(i) for i in fish_list[0][0:None]]
 
-new_list = []
+print("Starting fish_list", fish_list)
 
-for line in file_list:
-    new_list.append(line.strip())
-    print("new_list:", new_list)
+day_count = 1
+while day_count <= 80:
+    for i in range(len(fish_list)):
+        # print("fish is:", fish_list[i])
+        if fish_list[i] == 0:
+            fish_list.append(8)
+            fish_list[i] = 7
+        fish_list[i] -= 1
+    # print("updated fish_list after day:", day_count, " is:", fish_list)
+    day_count += 1
+
+print("Number of fish:", len(fish_list))
