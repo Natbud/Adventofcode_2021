e5932c896b58cadde43e666528c8dbdadfb05bc2
Natbud <96924057+Natbud@users.noreply.github.com>
2022-01-21 00:33:27 +0000

    Update 08_02.py

--- 08_02.py
+++ 08_02.py
@@ -19,35 +19,41 @@ print(file_list_output)
 
 word_lengths = []
 
-for output in file_list_output[0:None]:
-    for word in output.split():
+for digit in file_list_digits[0:None]:
+    for word in digit.split():
         # print(word)
-        word_lengths.append(len(word))
+        word_length = (len(word))
+        # check for 1, 4, 7 and 8 Unique number of segments for these:
+        if word_length == 2:
+            digit_value = 1
+        if word_length == 4:
+            digit_value = 4
+        if word_length == 3:
+            digit_value = 7
+        if word_length == 7:
+            digit_value = 8
+        # check for 0, 6, 9 (all length 6)
+        if word_length == 6 and "f" not in word:
+            digit_value = 0
+        if word_length == 6 and "a" not in word:
+            digit_value = 6
+        if word_length == 6 and "g" not in word:
+            digit_value = 9
 
-# print(word_lengths)
-
-digit_value = 0
-
-for length in word_lengths:
-    # check for 1, 4, 7 and 8 Unique number of segments for these:
-    if length == 2:
-        digit_value = 1
-    if length == 4:
-        digit_value = 4
-    if length == 3:
-        digit_value = 7
-    if length == 7:
-        digit_value = 8
+        # check for 2, 3, 5 (all length 5) - THIS ISN'T WORKING......
+        matches1 = ["e", "b"]
+        if word_length == 5 and all(x not in word for x in matches1):
+            digit_value = 2
+        matches2 = ["e", "g"]
+        if word_length == 5 and all(x not in word for x in matches2):
+            digit_value = 3
+        matches3 = ["a", "g"]
+        if word_length == 5 and all(x not in word for x in matches3):
+            digit_value = 5
 
 
-    # check for 0, 6, 9 (all length 6)
-    if length == 6:
-        # check if section f, g or a is missing
-        pass
-
-    print(digit_value)
-
-
+        print(word, digit_value)
+    print("digit just done:", digit)
 
 
 
