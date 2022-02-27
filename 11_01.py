9587fbb6530571d096a4795da4c4c1d450db6b49
Natbud <96924057+Natbud@users.noreply.github.com>
2022-02-27 12:57:30 +0000

    11_01_finished

--- 11_01.py
+++ 11_01.py
@@ -1,6 +1,7 @@
 import numpy as np
+import re as re
 
-filepath = "11_01_Test_Data.txt"
+filepath = "11_01_Data.txt"
 
 # using splitlines like this is good as it splits on the newline character
 # which essentially removes it from each line during reading.
@@ -34,7 +35,7 @@ print("np_grid before any steps:\n", np_grid)
 # Set number of steps to execute:
 max_steps = 100
 step = 0
-
+flashes_count = 0
 while step < max_steps:
     # START A 'STEP':
     step += 1
@@ -50,49 +51,64 @@ while step < max_steps:
     flashed_list = []
 
     # Substep 2: 'Flash any octopus/value greater than 9'
-    # while not np.all(np_grid < 10):
+    # Something here to ONLY check indicies NOT in flashed_list.
+    while np.any(np_grid >= 10):
     # while not np.less(np.all(np_grid), 10):
-    for r, row in enumerate(np_grid):
-        for d, digit in enumerate(row):
-            if digit >= 10 and [r,d] not in flashed_list:
-                # add co-ordinate to flashed_list
-                flashed_list.append([r,d])
-                # element flashes - increase all 8 adjacent (incl diags) by 1:
-                # NSEW:
-                try:
-                    np_grid[r][d+1] +=1
-                except:
-                    pass
-                try:
-                    np_grid[r+1][d] +=1
-                except:
-                    pass
-                try:
-                    np_grid[r][d-1] +=1
-                except:
-                    pass
-                try:
-                    np_grid[r-1][d] +=1
-                except:
-                    pass
-                try:
-                # DIAGS:
-                    np_grid[r-1][d+1] +=1
-                except:
-                    pass
-                try:
-                    np_grid[r+1][d+1] +=1
-                except:
-                    pass
-                try:
-                    np_grid[r-1][d-1] +=1
-                except:
-                    pass
-                try:
-                    np_grid[r+1][d-1] +=1
-                except:
-                    pass
+        for r, row in enumerate(np_grid):
+            for d, digit in enumerate(row):
+                if digit >= 10 and [r,d] not in flashed_list:
+                    # add co-ordinate to flashed_list
+                    flashed_list.append([r,d])
+                    # element flashes - increase all 8 adjacent (incl diags) by 1:
+                    # NSEW:
+                    # EAST
+                    if not d == 9:
+                        np_grid[r][d+1] +=1
+                    else:
+                        pass
+                    # SOUTH
+                    if not r ==9:
+                        np_grid[r+1][d] +=1
+                    else:
+                        pass
+                    # WEST
+                    # Buid in my own 'catch for a value on left side of board
+                    # because it just loops / affects right side otherwise:
+                    if d == 0:
+                        pass
+                    else:
+                        np_grid[r][d-1] +=1
+                    # NORTH
+                    # And another if row is zero, loops to affect bottom of GRID
+                    # so build in own catch:
+                    if r == 0:
+                        pass
+                    else:
+                        np_grid[r-1][d] +=1
 
+                    # NORTH EAST:
+                    if r == 0 or d == 9:
+                        pass
+                    else:
+                        np_grid[r-1][d+1] +=1
+                    # SOUTH EAST
+                    if r == 9 or d == 9:
+                        pass
+                    else:
+                        np_grid[r+1][d+1] +=1
+                    # NORTH WEST
+                    if r == 0 or d == 0:
+                        pass
+                    else:
+                        np_grid[r-1][d-1] +=1
+                    # SOUTH WEST
+                    if d == 0 or r == 9:
+                        pass
+                    else:
+                        np_grid[r+1][d-1] +=1
+                    np_grid[r][d] = -100
+                    flashes_count += 1
+                    # print("FLASH HAPPENED\n",re.sub('[\[\]]', '', np.array_str(np_grid)))
 
     # Now need to FLASH any others that have reached 10 or more....recursive?
     # Add flashed positions to a list so those positions can't be flashed agian
@@ -104,8 +120,11 @@ while step < max_steps:
     # finally set all octopuses with values greater than 9 to 0:
     for r, row in enumerate(np_grid):
         for d, digit in enumerate(row):
-            if digit > 9:
+            # if digit > 9:
+            if digit < 0:
                 np_grid[r][d] = 0
             # now make sure that
 
     print("np_grid after zeroes added after step:", step, "\n", np_grid)
+
+print("Total Number of Flashes in", step, "steps:", flashes_count)
