4043017c5597261559629806b9653e8dcbb52c30
Natbud <96924057+Natbud@users.noreply.github.com>
2022-03-05 13:35:19 +0000

    Update 12_01.py

--- 12_01.py
+++ 12_01.py
@@ -25,17 +25,34 @@ print("nodes:", nodes)
 
 # Find children of each node and add to a list:
 for node in nodes:
-    print("\ncurrent node being checked: ", node)
+    print("\ncurrent node being checked:", node)
     children = []
     for pair in file_list:
         partitioned_string = pair.partition("-")
         print("Partitioned String: ", partitioned_string)
-        if partitioned_string[0] is not node and partitioned_string[2] is node:
+        if partitioned_string[0] != node and partitioned_string[2] == node:
             children.append(partitioned_string[0])
             print("child appended: ", partitioned_string[0])
         else:
-            if partitioned_string[2] is not node and partitioned_string[0] is node:
+            if partitioned_string[2] != node and partitioned_string[0] == node:
                 children.append(partitioned_string[2])
                 print("child appended: ", partitioned_string[2])
 
-    print("node: ", node, "\n children:", children, "\n")
+    print("node:",node, "\n children:", children, "\n")
+
+    # Now add the node and chlidren to an adjacency list dictionary:
+    graph_dict[node] = children
+    print("graph_dict so far:", graph_dict)
+
+# Now do Depth First Search of Graph:
+visited = set() # to keep track of visited nodes
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+#Driver code for dfs function:
+print("Following is the Depth First Search")
+dfs(visited, graph_dict, "start")
