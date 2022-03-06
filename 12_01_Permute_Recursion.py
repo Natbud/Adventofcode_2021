faa2a1f19237d9064c2bdd172183f52833813abe
Natbud <96924057+Natbud@users.noreply.github.com>
2022-03-06 23:53:00 +0000

    Update 12_01_Permute_Recursion.py

--- 12_01_Permute_Recursion.py
+++ 12_01_Permute_Recursion.py
@@ -1,15 +1,39 @@
+import nltk
+from nltk.corpus import words
+#nltk.download('words')
+
+#d = enchant.Dict()
+
+
+anagrams = []
 
 def rec_permute(sofar, rest):
+    # This checks if a string has been generated from all the characters:
+    # If so, prints the current permutation/anagram:
     if rest == "":
-        print(sofar, exit())
+        # print(sofar)
+        anagrams.append(sofar)
     else:
+        #takes the lengths of the initial character string 'rest'
         for i in range(len(rest)):
+            # adds a character from 'rest' to the new 'sofar' (whic his now called 'next')
             next = sofar + rest[i]
-            remaining = rest[0:i-1]
+            # sets remaiining (the new 'rest' to everything before and after 'i'
+            # in rest.  so removes 'i' from 'rest'
+            remaining = rest[:i] + rest[i+1:]
             rec_permute(next, remaining)
 
-
 #driver code:
-rest = "abcd"
-#input("please enter string:")
+rest = "meats"
 rec_permute("", rest)
+
+#PRINT RESULTS:
+print("checking anagrams with dictionary.....")
+count = 1
+for i in set(anagrams):
+    # check if anagram is in dictionary
+    if i in words.words():
+        print(count, i)
+    count +=1
+print("number of anagrams including duplicates:", len(anagrams))
+print("number of anagrams without duplicates:", len(set(anagrams)))
